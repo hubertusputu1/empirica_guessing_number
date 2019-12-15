a36b0858454992b0ad7dad8e58c3e73299106a72
hubertus.putu1 <hubertus.putu1@gmail.com>
2019-12-15 22:01:36 +0700

    first commit

---
+++ client/game/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class About extends React.Component {
+  render() {
+    return <div>You must guess a number between 0 and 100. Each round, you make a guess, and
+    you will be told if your guess was above the number to guess, below the number
+    to guess, or correct.</div>;
+  }
+}
---
+++ client/intro/InstructionStepOne.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Centered } from "meteor/empirica:core";
+
+export default class InstructionStepOne extends React.Component {
+  render() {
+    const { hasPrev, hasNext, onNext, onPrev, game } = this.props;
+
+    return (
+      <Centered>
+        <div className="instructions">
+          <h1> Instructions</h1>
+          <p>
+            You must guess a number between 0 and 100. Each round, you make a guess, and
+            you will be told if your guess was above the number to guess, below the number
+            to guess, or correct.
+          </p>
+
+          <p>
+            <button type="button" onClick={onNext} disabled={!hasNext}>
+              Next
+            </button>
+          </p>
+        </div>
+      </Centered>
+    );
+  }
+}
